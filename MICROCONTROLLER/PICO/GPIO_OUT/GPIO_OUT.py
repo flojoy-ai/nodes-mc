a82f2be635ec6905e1899ff0c73b41895815a9bd
Hristo <sandevh@gmail.com>
2023-11-08 13:31:46 -0500

    GPIO  out node

---
+++ MICROCONTROLLER/PICO/GPIO_OUT/GPIO_OUT.py
@@ -0,0 +1,38 @@
+from typing import Literal, Optional, cast
+from flojoy import DataContainer, JobResultBuilder, flojoy
+
+@flojoy
+def GPIO_OUT(
+    default: Optional[DataContainer] = None,
+    pin: Literal[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+                    18, 19, 20, 21, 22, 26, 27, 28] = 1,
+    mode: Literal["Toggle", "On", "Off"] = "Toggle",
+) -> DataContainer:
+    """The PIN_OUT node specifies voltage output through the specified PIN \
+        on a Rasbperry Pico. This is a GPIO (General Purpose Input/Output).
+
+    Parameters
+    ----------
+    pin: specifies the pin to send out voltage
+    mode: specifies the mode of the LED,
+    - on: turns the LED on
+    - off: turns the LED off
+    - toggle: toggles the LED (on -> off, off -> on)
+    """
+    from machine import Pin  # type: ignore
+
+    selected = Pin(pin, Pin.OUT)
+
+    if mode == "On":
+        selected.value(1)
+    elif mode == "Off":
+        selected.value(0)
+    elif mode == "Toggle":
+        selected.toggle()
+
+    result = cast(
+        DataContainer,
+        JobResultBuilder().from_inputs([default] if default else []).build(),
+    )
+
+    return result
